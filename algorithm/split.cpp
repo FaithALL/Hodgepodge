e45dc85e00d3549650a72c4d8ceece47f280f4cf
guoqiang <1915763994@qq.com>
2021-02-08 13:54:33 +0800

    添加分隔字符串split函数

---
+++ algorithm/split.cpp
@@ -0,0 +1,26 @@
+#include <string_view>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/**
+ * 见 https://www.zhihu.com/question/36642771/answer/865135551
+ * @param str 待分隔的字符串
+ * @param delimiters 分隔符
+ * @param tokens 分隔后存放位置
+ */
+void split(string_view str, string_view delimiters, vector<string_view>& tokens) {
+    string_view::size_type lastPos = str.find_first_not_of(delimiters, 0);
+    string_view::size_type pos = str.find_first_of(delimiters, lastPos);
+    while (pos != string_view::npos || lastPos != string_view::npos) {
+        tokens.emplace_back(str.substr(lastPos, pos - lastPos));
+        lastPos = str.find_first_not_of(delimiters,pos);
+        pos = str.find_first_of(delimiters, lastPos);
+    }
+}
+
+int main() {
+    vector<string_view> tokens;
+    split(" i love you! ", " ", tokens);
+    for (auto x : tokens) cout << "**"<< x << "**" << endl;
+}
