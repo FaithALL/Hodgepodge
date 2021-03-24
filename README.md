22e5c0bdca1f90ad00995d053f71572a6362bf42
guoqiang <1915763994@qq.com>
2021-03-24 17:00:52 +0800

    LRU算法

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
   * [cpy.c](algorithm/cpy.c)：c语言中的`memcpy`和`strcpy`
   * [sort.c](algorithm/sort.cpp)：冒泡、选择、插入、归并、堆排、快排
   * [split](algorithm/split.cpp)：字符串分割
-  * [cache_replacement](algorithm/cache_replacement.cpp)：缓存淘汰算法`LRU`、`LFU`
+  * [cache_replacement](algorithm/cache_replacement.cpp)：缓存淘汰算法`LRU`
 
--- algorithm/cache_replacement.cpp
+++ algorithm/cache_replacement.cpp
@@ -1,2 +1,40 @@
+#include <list>
+#include <unordered_map>
 
+using namespace std;
+
+//https://leetcode-cn.com/problems/lru-cache-lcci/
+class LRUCache {
+    using iter = list<pair<int, int>>::iterator;
+    list<pair<int, int>> doubly_list;
+    unordered_map<int, iter> hash_map;
+    int cap;
+public:
+    explicit LRUCache(int capacity) : cap(capacity) {
+    }
+
+    optional<int> get(int key) {
+        if (hash_map.find(key) == hash_map.end()) return std::nullopt;
+        doubly_list.push_front(std::move(*hash_map[key]));
+        doubly_list.erase(hash_map[key]);
+        hash_map[key] = doubly_list.begin();
+        return hash_map[key]->second;
+    }
+
+    void put(int key, int value) {
+        if (hash_map.find(key) == hash_map.end()) {
+            doubly_list.emplace_front(key, value);
+            hash_map[key] = doubly_list.begin();
+            if (hash_map.size() > cap) {
+                hash_map.erase(doubly_list.back().first);
+                doubly_list.pop_back();
+            }
+        } else {
+            doubly_list.push_front(std::move(*hash_map[key]));
+            doubly_list.erase(hash_map[key]);
+            hash_map[key] = doubly_list.begin();
+            hash_map[key]->second = value;
+        }
+    }
+};
 
